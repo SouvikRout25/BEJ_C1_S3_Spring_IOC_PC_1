1543a08c0b83c2f4d8205a09fc7c6e2f299f8179
SOUVIK ROUT <souvikrout.21@gmail.com>
2022-10-31 16:28:58 +0530

    done

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -1,2 +1,20 @@
-package org.example;public class AppConfig {
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+
+public class AppConfig {
+
+    @Bean("employee")
+    public Employee getEmployeeDetails(){
+        Employee employee = new Employee();
+        employee.setEmpId(1045);
+        employee.setEmpName("Blaziken Getsugatnsho");
+        employee.setSalary(750000.50);
+        return employee;
+    }
+
+    @Bean
+    public Department getDepartmentDetails(){
+        return new Department(145,"Soul Reaper");
+    }
 }
--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,2 +1,63 @@
-package org.example;public class Employee {
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+    private int empId;
+    private String empName;
+    private double salary;
+    @Autowired
+    private Department department;
+
+    public Employee() {
+    }
+
+    public Employee(int empId, String empName, double salary, Department department) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", salary=" + salary +
+                ", department=" + department +
+                '}';
+    }
 }
